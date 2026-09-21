f28760be3c0b6e5212ce37234fb76ea0b0d7158c
Adarsh Tiwari <adarsh15805@gmail.com>
2026-09-21 19:31:02 +0000

    Sep 22, 2026, 1:01 AM

--- 02_first.py
+++ 02_first.py
@@ -50,6 +50,6 @@ print("and operator", a==b and a>b)#jab dono true ho to true return karega other
 print("or operator", a==b or a>b)# jab dono false ho to false return karega otherwise true return karega
 print("and operator", a==b and a<b)#jab dono true ho to true return karega otherwise false return karega
 print("or operator", a==b or a<b)# jab dono false ho to false return karega otherwise true return karega
-"""""
+"""
 
-input1=int(input("Enter name : "))
+(input("Enter name : "))
