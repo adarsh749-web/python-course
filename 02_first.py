b15d5c51e776a0bb2bd8cf8b271a8ff96e4d8782
Adarsh Tiwari <adarsh15805@gmail.com>
2026-09-21 04:59:59 +0000

    Sep 21, 2026, 10:29 AM

--- 02_first.py
+++ 02_first.py
@@ -1,5 +1,16 @@
-name = "Adarsh"
+"""name = "Adarsh"
 age = 20
 
 print("My name is", name)
-print("My age is", age)
+print("My age is", age)"""""
+
+
+a=5
+b=2
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)### remainder (modulas)
+
